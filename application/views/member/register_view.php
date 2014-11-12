bd11938c0725d56da4b58427d202337939d1e489
engineerball <engineerball@gmail.com>
2014-11-12 22:49:57 +0700

    Add member views

---
+++ application/views/member/register_view.php
@@ -0,0 +1,18 @@
+			<form role="form">
+				<div class="form-group">
+					 <label for="fullname">Full Name</label><input type="text" class="form-control" id="fullname">
+				</div>
+				<div class="form-group">
+					 <label for="idcard">ID Card</label><input type="text" class="form-control" id="idcard">
+				</div>
+				<div class="form-group">
+					 <label for="username">Username</label><input type="text" class="form-control" id="username">
+				</div>
+				<div class="form-group">
+					 <label for="idcard">Password</label><input type="password" class="form-control" id="password">
+				</div>
+				<div class="form-group">
+					 <label for="exampleInputEmail1">Email address</label><input type="email" class="form-control" id="exampleInputEmail1">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
