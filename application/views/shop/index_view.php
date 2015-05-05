a439a8a94aa7e81343df34d19cc2837426ba58b0
engineerball <ball@engineerball.com>
2015-05-05 11:29:40 +0700

    edit can clickable on image at firstpage

--- application/views/shop/index_view.php
+++ application/views/shop/index_view.php
@@ -126,7 +126,7 @@
 	foreach ($products as $item){
 ?>
 						<div class="col-md-4 column">
-							<img alt="140x140" src="<?php echo base_url()."assets/uploads/files/products/".$item->images; ?>" />
+							<a href="<?php echo base_url().'shop2/product/'.$item->id ; ?>" ><img alt="140x140" src="<?php echo base_url()."assets/uploads/files/products/".$item->images; ?>" /></a>
 							<dl>
 								<dt>
                                 <a href="<?php echo base_url().'shop2/product/'.$item->id ; ?>" ><?php echo $item->name; ?></a>
